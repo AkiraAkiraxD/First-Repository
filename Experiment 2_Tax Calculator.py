99c23d85eb8e72d465ff69bb64419a0c6da362a3
Akira <124269519+AkiraAkiraxD@users.noreply.github.com>
2023-02-08 18:58:42 +0800

    Upload: Experiment 2_Tax Calculator

---
+++ Experiment 2_Tax Calculator.py
@@ -0,0 +1,27 @@
+
+def taxCompute(money):
+    if money <= 250000:
+        pay = money
+    elif money > 250000 and money <= 400000:
+        pay = money * 0.15
+    elif money > 400000 and money <= 800000:
+        pay = 22500 + (money - 400000) * 0.2
+    elif money > 800000 and money <= 2000000:
+        pay = 102500 + (money - 800000) * 0.25
+    elif money > 2000000 and money <= 8000000:
+        pay = 402500 + (money - 2000000) * 0.3
+    elif money > 8000000:
+        pay = 2202500 + (money - 8000000) * 0.35
+    else:
+        print("========== INVALID INPUT ==========")
+    
+    return (pay)
+
+
+income = float(input("Enter your monthly income: "))
+annual = income * 12
+taxRate = taxCompute(annual)
+print("========== RECEIPT ==========")
+print("Monthly Income: {}" .format(income))
+print("Annual Income: {}" .format(annual))
+print("Tax Rate: {}" .format(taxRate))
