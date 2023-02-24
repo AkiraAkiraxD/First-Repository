82c3857ea9cbb91d724391a2a55f3aa539afdf12
Akira <124269519+AkiraAkiraxD@users.noreply.github.com>
2023-02-24 13:45:33 +0800

    Finishing the TextBasedGame
    Finished the game!
    - disabled animation (for now)
    - alternate attack move working
    - changed "defend" to "ultimate" with its own charge system
    - fully working charge system 
    - added "try except" statements to avoid user inputting wrong integers
    - added win/lose end screen
    - added loading time to let users read and process the information
    - enemy now attacks the player with a random damage multiplier
    - attacks and heals can now be amplified by multiplying it on RNG
    - added heals and charge system cap

--- TextBasedGame_Experiment.py
+++ TextBasedGame_Experiment.py
@@ -3,22 +3,39 @@ import random
 
 # list = [you(0),enemy(1)]
 hitPoints = [50,100]
-damage = [3,1]
+damage = [3,1,10]
+charge = 0
 game = 1
+attacker = "you"
 
+def ultimateCheck():
+    global charge
+    charge+=1
+    if charge > 5:
+        charge=5
+def wait():
+    print("Loading...")
+    time.sleep(random.randint(1,2))
 def reset():
     damage[0] = 3
+    damage[1] = 1
 def action(num):
+    global charge
+    wait()
     match num:
         case 1:
+            switch()
+            ultimateCheck()
             dice = random.randint(1,3)
             damage[0] *= dice
             hitPoints[1] -= damage[0]
-            animation()
+            #animation()
             main()
             print("You rolled {} and dealt {} damage!" .format(dice,damage[0]))
             reset()
         case 2:
+            switch()
+            ultimateCheck()
             dice = random.randint(3,10)
             hitPoints[0] += dice
             if hitPoints[0] > 50:
@@ -26,7 +43,19 @@ def action(num):
             main()
             print("You healed {} health!" .format(dice))
         case 3:
-            print()
+            if charge == 5:
+                switch()
+                hitPoints[1] -= damage[2]
+                #animation()
+                main()
+                print("YOU CASTED YOUR ULTIMATE! Dealing {} damage!" .format(damage[2]))
+                charge = 0
+            else:
+                main()
+                print("Not enough charge!")
+        case _:
+            main()
+    gameCheck()
 def diffSetting(num):
     match num:
         case 1:
@@ -39,6 +68,8 @@ def diffSetting(num):
             hitPoints[1] *= 1.5
         case 5:
             hitPoints[1] *= 2.0
+        case _:
+            start()
 def main():
     slide_1 = '''
     HP: {}          HP: {}
@@ -48,7 +79,7 @@ def main():
     |  |        .     \ \/ /|
     | / \       .      \  / |
     |           .       \/  |
-    /1 = Attack/2 = Heal/3 = Defend/''' .format(hitPoints[0],hitPoints[1])
+    |1 = Attack|2 = Heal|3 = Ultimate(Charge: {}/5)|''' .format(hitPoints[0],hitPoints[1],charge)
     print(slide_1)
 def animation():
     slide_1 = '''
@@ -137,21 +168,58 @@ def start():
         diffSetting(diff)
     except ValueError:
         start()
+def enemyMove():
+    wait()
+    time.sleep(2)
+    switch()
+    dice = random.randint(1,5)
+    damage[1] *= dice
+    hitPoints[0] -= damage[1]
+    main()
+    print("Diamond rolled {} and dealt {} damage!" .format(dice,damage[1]))
+    reset()
+    gameCheck()
+def switch():
+    global attacker
+    match attacker:
+        case "you":
+            attacker = "enemy"
+        case "enemy":
+            attacker = "you"
+def gameCheck():
+    global game
+    if hitPoints[0] < 1:
+        game = 0
+        print('''
+    |========================|
+    |                        |
+    |       YOU LOST         |
+    |        reason:         |
+    |     (skill issue)      |
+    |                        |
+    |========================|''')
+    elif hitPoints[1] < 1:
+        game = 0
+        print('''
+    |========================|
+    |                        |
+    |       YOU WON?         |
+    |      reported:         |
+    |   (suspected hacks)    |
+    |                        |
+    |========================|''')
+
 
 #================== GAME STARTS HERE ==================
-
 start()
 main()
-
 while game == 1:
-    try:
-        move = int(input("Action: "))
-        action(move)
-    except ValueError:
-        main()
-    
-input()
-    
-
-
-print("Game Done")
+    if attacker == "you":
+        try:
+            move = int(input("Action: "))
+            action(move)
+        except ValueError:
+            main()
+    elif attacker == "enemy": 
+        print("Enemy's Turn")
+        enemyMove()
