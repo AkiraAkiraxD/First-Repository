0a8d73bc1bd75c3b17bdf2f86fe31adb84e61d80
Akira <124269519+AkiraAkiraxD@users.noreply.github.com>
2023-02-27 16:57:58 +0800

    Add files via upload
    eys assignment 1 poggers

---
+++ script_1.py
@@ -0,0 +1,12 @@
+def celsius(x):
+    global ans
+    ans = (x-32)*5/9
+def fahrenheit(x):
+    global ans
+    ans = x*(9/5)+32
+def kelvin(x):
+    global ans
+    ans = x+273.15
+def k2c(x):
+    global ans
+    ans = x-273.15
